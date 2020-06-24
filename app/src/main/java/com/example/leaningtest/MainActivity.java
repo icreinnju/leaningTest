c2dbe492d178d2a205a2b076146138b122a1061f
GAOHH <huanhuan.gao@basf.com>
2020-06-24 10:16:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/leaningtest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.leaningtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv_hello = findViewById(R.id.tv_hello);
+        tv_hello.setText("今天实施");
+        tv_hello.setTextColor(Color.RED);
+        tv_hello.setTextSize(30);
+    }
+}
